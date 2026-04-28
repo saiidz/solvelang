0082ffaf17c28f97d10b899d3fabe17bfee627e9
saiidz <saiidzeidan@gmail.com>
2026-04-28 09:03:18 -0400

    Wire parser lexer and value modules

--- solvec/src/main.rs
+++ solvec/src/main.rs
@@ -1,5 +1,9 @@
+mod ast;
 mod eval;
+mod lexer;
+mod parser;
 mod runtime;
+mod value;
 
 use std::env;
 use std::fs;
@@ -14,6 +18,20 @@ fn main() {
 
     let filename = &args[1];
     let content = fs::read_to_string(filename).expect("Failed to read file");
+
+    if args.iter().any(|arg| arg == "--tokens") {
+        let tokens = lexer::lex(&content);
+        println!("{:#?}", tokens);
+        return;
+    }
+
+    if args.iter().any(|arg| arg == "--ast") {
+        let tokens = lexer::lex(&content);
+        let mut parser = parser::Parser::new(tokens);
+        let ast = parser.parse();
+        println!("{:#?}", ast);
+        return;
+    }
 
     runtime::run(&content);
 }
