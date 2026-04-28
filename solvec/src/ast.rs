ba2bacc0ba206b03c7cb913220faa4738c7cbf85
saiidz <saiidzeidan@gmail.com>
2026-04-28 09:02:38 -0400

    Add AST foundation

---
+++ solvec/src/ast.rs
@@ -0,0 +1,67 @@
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expr {
+    Number(i32),
+    Text(String),
+    Bool(bool),
+    Variable(String),
+    Array(Vec<Expr>),
+    Index(Box<Expr>, Box<Expr>),
+    Binary {
+        left: Box<Expr>,
+        operator: BinaryOp,
+        right: Box<Expr>,
+    },
+    Call {
+        name: String,
+        args: Vec<Expr>,
+    },
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum BinaryOp {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Join,
+    Equal,
+    NotEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Stmt {
+    Let {
+        name: String,
+        value: Expr,
+    },
+    Print(Expr),
+    Return(Expr),
+    Function {
+        name: String,
+        params: Vec<String>,
+        body: Vec<Stmt>,
+    },
+    If {
+        condition: Expr,
+        then_branch: Vec<Stmt>,
+        else_branch: Vec<Stmt>,
+    },
+    While {
+        condition: Expr,
+        body: Vec<Stmt>,
+    },
+    Agent {
+        name: String,
+        instruction: String,
+        tools: Vec<String>,
+    },
+    Ask {
+        agent: String,
+        message: Expr,
+    },
+    Expr(Expr),
+}
