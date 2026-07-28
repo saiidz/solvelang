0ee7c733e61e4cfe80b75190591e7d2082b47ea2
saiidz <saiidzeidan@gmail.com>
2026-07-28 14:04:34 -0400

    Wire test-mode Stripe subscription lifecycle

--- services/api-access/src/handler.js
+++ services/api-access/src/handler.js
@@ -1,9 +1,13 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import Stripe from "stripe";
 import { createApiAccessHandler } from "./api-handler.js";
 import { parseApiAccessEnvironment } from "./config.js";
 import { createDynamoApiAccessStore } from "./dynamo-store.js";
 import { createApiAccessService } from "./service.js";
+import { createStripeSubscriptionGateway } from "./stripe-subscriptions.js";
+import { createDynamoSubscriptionEventStore } from "./subscription-event-store.js";
+import { createSubscriptionCheckoutService, createSubscriptionLifecycleService } from "./subscriptions.js";
 
 const environment = parseApiAccessEnvironment(process.env);
 const documentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
@@ -13,11 +17,35 @@ const service = createApiAccessService({
   pepper: environment.pepper,
   mode: environment.mode,
 });
+
+let stripeGateway;
+let subscriptionCheckout;
+let subscriptionLifecycle;
+if (environment.subscriptionBillingEnabled) {
+  const stripe = new Stripe(environment.stripeSecretKey, { apiVersion: "2026-06-24.dahlia" });
+  stripeGateway = createStripeSubscriptionGateway(stripe, environment.stripeWebhookSecret);
+  subscriptionCheckout = createSubscriptionCheckoutService({
+    gateway: stripeGateway,
+    priceIds: environment.priceIds,
+    siteOrigin: environment.siteOrigin,
+    enabled: true,
+  });
+  subscriptionLifecycle = createSubscriptionLifecycleService({
+    apiAccessService: service,
+    eventStore: createDynamoSubscriptionEventStore(documentClient, environment.subscriptionEventsTable),
+    priceIds: environment.priceIds,
+  });
+}
+
 const application = createApiAccessHandler({
   service,
   enabled: environment.enabled,
   adminSecret: environment.adminSecret,
   siteOrigin: environment.siteOrigin,
+  subscriptionBillingEnabled: environment.subscriptionBillingEnabled,
+  subscriptionCheckout,
+  subscriptionLifecycle,
+  stripeGateway,
 });
 
 export async function handler(event) {
