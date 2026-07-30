c4aa86efc25d206a740c1471980510e948c9cf0b
saiidz <saiidzeidan@gmail.com>
2026-07-29 20:09:24 -0400

    Add priority dispatcher Lambda handler

---
+++ services/api-access/src/priority-dispatcher-handler.js
@@ -0,0 +1,25 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { parsePriorityDispatcherEnvironment } from "./priority-config.js";
+import { createPriorityDispatcher } from "./priority-dispatcher.js";
+import { createDynamoPriorityJobStore } from "./priority-job-store.js";
+
+const environment = parsePriorityDispatcherEnvironment(process.env);
+const documentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+const sqs = new SQSClient({});
+const application = createPriorityDispatcher({
+  queueGateway: {
+    async send({ queueUrl, messageBody }) {
+      const response = await sqs.send(new SendMessageCommand({ QueueUrl: queueUrl, MessageBody: messageBody }));
+      if (!response.MessageId) throw new Error("SQS did not return a message ID.");
+      return { messageId: response.MessageId };
+    },
+  },
+  jobStore: createDynamoPriorityJobStore(documentClient, { jobsTable: environment.priorityJobsTable }),
+  queueUrls: environment.queueUrls,
+});
+
+export async function handler(event) {
+  return application(event);
+}
