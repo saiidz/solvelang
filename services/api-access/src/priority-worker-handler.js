b50268a71003d200443a59ad27057deadee0e44d
saiidz <saiidzeidan@gmail.com>
2026-08-15 16:59:28 -0400

    feat(priority): inject provider executor only when enabled

--- services/api-access/src/priority-worker-handler.js
+++ services/api-access/src/priority-worker-handler.js
@@ -1,9 +1,12 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { createDynamoAccountAccessReader } from "./account-access-reader.js";
 import { createAccountAccessService } from "./account-access.js";
 import { parsePriorityWorkerEnvironment } from "./priority-config.js";
 import { createDynamoPriorityJobStore } from "./priority-job-store.js";
+import { createHttpsPriorityProviderExecutor } from "./priority-provider-executor.js";
+import { createS3PrioritySourceStore } from "./priority-source-store.js";
 import { createPriorityWorker } from "./priority-worker.js";
 
 const environment = parsePriorityWorkerEnvironment(process.env);
@@ -13,10 +16,18 @@ const accountAccess = environment.customerAuthTable
       store: createDynamoAccountAccessReader(documentClient, { tableName: environment.customerAuthTable }),
     })
   : undefined;
+const executeCustomerJob = environment.providerExecutionEnabled
+  ? createHttpsPriorityProviderExecutor({
+      sourceStore: createS3PrioritySourceStore(new S3Client({}), { bucket: environment.sourceBucket }),
+      providerUrl: environment.providerUrl,
+      providerSecret: environment.providerSecret,
+    })
+  : undefined;
 const application = createPriorityWorker({
   laneName: environment.laneName,
   workerId: environment.workerId,
   accountAccess,
+  executeCustomerJob,
   jobStore: createDynamoPriorityJobStore(documentClient, { jobsTable: environment.priorityJobsTable }),
 });
 
